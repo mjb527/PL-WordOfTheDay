a92b9619828f5336cf04097cdf47b839da598337
Matt <mbatko0914@gmail.com>
2020-04-05 17:56:27 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PL-WordOfTheDay
+
+This is a module for a [Magic Mirror](https://github.com/MichMich/MagicMirror) that scrapes a Polish Word of the Day from [https://www.polishpod101.com/polish-phrases/](https://www.polishpod101.com/polish-phrases/) and formats.
+
+Includes:
+1. The word of the day
+2. The translation of the word
+3. The part of speech (noun, verb, etc.)
+4. Examples of how to use it in a sentence
+
+The examples are on a carousel, which rotates every 20 seconds. 
---
+++ webscrape.js
@@ -0,0 +1,51 @@
+
+// request modules
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const options = {
+  uri: `https://www.polishpod101.com/polish-phrases/`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+rp(options)
+    .then(function (data) {
+      const $ = cheerio.load(data._root.children[1].children);
+      // data = data._root.children[1].children;
+      // console.log(data[2]);
+      // data.forEach((item) => function() {
+      //   console.log(item);
+      // });
+      // rows of Polish and English translation
+      const pRows = $('.r101-wotd-widget__word');
+      const eRows = $('.r101-wotd-widget__english');
+
+      const word = formatText(pRows[0].children[0].data);
+      const translation = formatText(eRows[0].children[0].data);
+      const partOfSpeech = formatText($('.r101-wotd-widget__class').text().trim());
+
+      const examples = [];
+
+      console.log(`${word} - ${translation} - ${partOfSpeech}`);
+
+      for(let i = 1; i < pRows.length; i++) {
+        // get the text, capitalize first letter
+        let polish = formatText(pRows[i].children[0].data);
+        let english = formatText(eRows[i].children[0].data)
+
+        examples.push({"polish" : polish,
+         "english" : english});
+        console.log(examples[i-1].polish + ' - ' + examples[i-1].english);
+      }
+
+    })
+    .catch(function (err) {
+      console.log(err);
+        // REQUEST FAILED: ERROR OF SOME KIND
+    });
+
+function formatText(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
